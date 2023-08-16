9950bb8f3bf5aec68060f5387908360b9c85d9cc
Paul-Hi <32524715+Paul-Hi@users.noreply.github.com>
2023-08-16 19:51:06 +0200

    disney diffuse + thin done -- will continue with everything else

--- assets/shaders/src/disney_bsdf.glsl
+++ assets/shaders/src/disney_bsdf.glsl
@@ -19,7 +19,7 @@ float Fresnel(in float F0, in float x)
     return F0 + (1.0 - F0) * Schlick(x);
 }
 
-vec3 evaluateDisneyDiffuse(out float pdf)
+vec3 evaluateDisneyDiffuse(in bool thin, out float pdf)
 {
     pdf = 1.0;
 
@@ -29,6 +29,17 @@ vec3 evaluateDisneyDiffuse(out float pdf)
     // lambert
     float fLambert = 1.0;
 
+    // thin
+    // this is some ss approximation by Burley based on Hanrahan Krueger
+    float fSSApprox = 0.0;
+    if(thin && surfaceState.bsdf.flatness > 0.0)
+    {
+        float Fss90 = surfaceState.hDotL * surfaceState.hDotL * surfaceState.bsdf.roughness;
+        float Fss = mix(1.0, Fss90, FL) * mix(1.0, Fss90, FV);
+        float ss = 1.25 * (Fss * (1.0 / (surfaceState.nDotL + surfaceState.nDotL) - 0.5) + 0.5);
+        fSSApprox = ss;
+    }
+
     float energyBias = mix(0.0, 0.5, surfaceState.bsdf.roughness) ;
     float energyFactor = mix(1.0, 1.0 / 1.51, surfaceState.bsdf.roughness) ;
     float RR = energyBias + 2.0 * surfaceState.bsdf.roughness * surfaceState.hDotL * surfaceState.hDotL;
@@ -36,7 +47,9 @@ vec3 evaluateDisneyDiffuse(out float pdf)
     // retro
     float fRetro = RR * (FL + FV + FL * FV * (RR - 1.0)) * energyFactor;
 
-    float fDisneyDiffuse = fLambert * (1.0 - 0.5 * FL) * (1.0 - 0.5 * FV) + fRetro;
+    float diffuseF = mix(fLambert, fSSApprox, thin ? surfaceState.bsdf.flatness : 0.0);
+
+    float fDisneyDiffuse = diffuseF * (1.0 - 0.5 * FL) * (1.0 - 0.5 * FV) + fRetro;
 
     // 1 / pdf cancels out with  * INV_PI * surfaceState.nDotL
     // pdf = surfaceState.nDotL * INV_PI;
